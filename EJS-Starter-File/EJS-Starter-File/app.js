f13352bdc1d0890e3d9506182f0b1eee7f9771a7
Tavishi Seth <tavishiseth@gmail.com>
2023-06-27 15:07:54 +0530

    initial commit

---
+++ EJS-Starter-File/EJS-Starter-File/app.js
@@ -0,0 +1,35 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+var items = ["Cook food", "Gym", "Read book"];
+
+app.get("/", function(req, res){
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+
+  if (currentDay === 6 || currentDay === 0) {
+    day = "Weekend";
+    res.sendFile(__dirname + "/weekend.html");
+  }
+  else {
+    day = "Weekday";
+    res.sendFile(__dirname + "/weekday.html");
+  }
+  res.render("list", {KindofDay: day, newListitem: items});
+});
+
+app.post("/", function(req, res) {
+  items.push(req.body.newItem);
+  res.redirect("/");
+})
+app.listen(3000, function(){
+  console.log("Server started on port 3000.");
+});
